3d2dfe19544c54035e8af1bc060fb3d6f0fc17cc
User <user@hackerrank.workspace>
2024-11-23 13:48:40 +0000

    Add initial repository
    Commit by HackerRank

---
+++ HRBackendExercise.API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using HRBackendExercise.API.Abstractions;
+using HRBackendExercise.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBackendExercise.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		// DO NOT MODIFY THE SIGNATURES, JUST THE BODIES
+
+        public ProductsController(IProductsService productsService)
+		{
+				
+        }
+
+        public IActionResult Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IActionResult Post(Product product)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IActionResult Put(Product product)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IActionResult Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ HRBackendExercise.API/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using HRBackendExercise.API.Abstractions;
+using HRBackendExercise.API.Models;
+
+namespace HRBackendExercise.API.Services
+{
+	public class ProductService : IProductsService
+	{
+		// DO NOT MODIFY THE SIGNATURES, JUST THE BODIES
+
+		public Product Create(Product entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Product? GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Product> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Product entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Product entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
